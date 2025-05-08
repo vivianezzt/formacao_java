4ff18161341427d533520b64b6ce313227296c32
viviane <vivianeaguiarc@outlook.com>
2025-05-08 17:34:22 -0300

    feat: java nivel 3 finalizado fundamentos de spring boot

---
+++ spring-projects/pprimeiro-projeto-spring/src/test/java/br/com/vivianeaguiardev/pprimeiro_projeto_spring/PprimeiroProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vivianeaguiardev.pprimeiro_projeto_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PprimeiroProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
